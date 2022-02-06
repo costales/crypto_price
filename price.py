7cb3714cde5fe0b316ff6b3113904a50bdbd962e
Costales <costales.marcos@gmail.com>
2022-02-06 10:38:35 +0100

    Updated version

--- price.py
+++ price.py
@@ -3,11 +3,12 @@
 # + Info: https://www.coinapi.io
 import requests, sys, datetime
 
-url = 'https://rest.coinapi.io/v1/exchangerate/$1/$2/history?period_id=1MIN&time_start=$3T$4:00&time_end=$3T$4:59'
+API = '<your_api_key_from_coinapi.io_here>'
+URL = 'https://rest.coinapi.io/v1/exchangerate/$1/$2/history?period_id=1MIN&time_start=$3T$4:00&time_end=$3T$4:59'
 
 def par(coin_from, coin_to, date, time):
-    url_api = url.replace('$1', coin_from).replace('$2', coin_to).replace('$3', date).replace('$4', time)
-    headers = {'X-CoinAPI-Key' : '<your_api_key_from_coinapi.io_here>'}
+    url_api = URL.replace('$1', coin_from).replace('$2', coin_to).replace('$3', date).replace('$4', time)
+    headers = {'X-CoinAPI-Key' : API}
     
     data = requests.get(url_api, headers=headers).json()
     return(data[0]['rate_close'])
